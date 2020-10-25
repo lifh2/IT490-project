b1828e151297529402d38c645957e0e831450b22
root <lih2@njit.edu>
2020-10-25 12:22:55 -0400

    modified:   login.php

--- login.php
+++ login.php
@@ -32,65 +32,24 @@ $response = $client->publish($request);
 echo "client received response: ";//.PHP_EOL;
 print_r($response);
 echo "\n\n";
+$server = new rabbitMQServer("testRabbitMQ.ini", "APA");
 
+$server->processrequests('requestProcessor');
+}
+
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
 
 sendRequest();
-/*
-function HandleLoginResponse(response)
-{
-	echo "inside handle login response...";
-        var text = JSON.parse(response);
-//      document.getElementById("textResponse").innerHTML = response+"<p>";
-        document.getElementById("textResponse").innerHTML = "response: "+text+"<p>";
-}
-
-function SendLoginRequest(username,password)
-{
-	echo "sending login request";
-        var request = new XMLHttpRequest();
-        request.open("POST","login.php",true);
-        request.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-        request.onreadystatechange= function ()
-        {
-                
-                if ((this.readyState == 4)&&(this.status == 200))
-                {
-                        HandleLoginResponse(this.responseText);
-                }               
-        }
-        request.send("type=login&uname="+username+"&pword="+password);
-}
-
-//SendLoginRequest($username,$password);
-
-//rabbitMQ code to pass/push into another server VM
-//
-//
-//return logic weather authenticion verified or not
-//print 
-
-if (!isset($_POST))
-{
-	$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
-	echo json_encode($msg);
-	exit(0);
-}
-$request = $_POST;
-$response = "unsupported request type, politely FUCK OFF";
-
-//switch ($request["type"])
-//{
-//	case "login":
-//		$response = "login, yeah we can do that";
-//	break;
-//}
-//
-echo json_encode($response);
-
-print 
-//exit(0);
- */
 ?>
 
 <html>
