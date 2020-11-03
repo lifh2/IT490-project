ece4bcfa350c05a49d8d185ef352b1dd4fd297e2
hanosifa <hanyo_tourza@hotmail.fr>
2020-11-03 00:32:54 -0500

    Sifaw modified this file

--- dbListener.php
+++ dbListener.php
@@ -4,7 +4,39 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 require_once('dbWorks.php');
+
+
 //do a function that uploal logs to a local file
+function errorlogger($msg)
+{
+          //path of the log file
+        $log_file = '/var/log/rabbit_log/log_rabbit.log';
+           //set error log to be active
+        ini_set("logr_errors", TRUE);
+          // setting the logging to be active
+        ini_set('error_log', $log_file);
+        // logging thhe error
+        $ermsg = print_r($msg, true);
+
+	error_log($ermsg);
+
+        $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+        echo "new client is created to sent error to broker\n";
+
+        $request = array();
+        $request['type'] = "logger";
+        $request['error'] = $ermsg;
+
+        var_dump($request);
+        $response = $client->send_request($request);
+        echo "error request has been sent to broker";
+        print_r($respons);
+
+}
+
+
+
+
 
 
 function requestProcessor($request)
@@ -18,18 +50,20 @@ function requestProcessor($request)
 	       	return array('message'=> "ERROR: unsupported message type");
 	}
 	 
+	//logger($request);
 
 
 	switch($request['type'])
 	{
 		case "Data":
-                          var_dump($request);
+                          
                           echo "data is found \n";
 			  print_r($request['name']);
 			  echo "\n";
 			  print_r($request['latestPrice']);
-			  
-                          break;
+			  echo "\n";
+			  return stockData($request['name'], $request['latestPrice']);
+                          
 
 
 		 case "login":
