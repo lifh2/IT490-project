7cc5e5342a9e80e3940c8f6de74855fca3b40037
root <lih2@njit.edu>
2020-11-13 16:04:19 -0500

    new file:   testLogger.php 	modified:   logger.php 	modified:   testRabbitMQ.ini

---
+++ testLogger.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+
+
+<?php
+require_once('/home/luke/IT490-project/logger.php');
+
+errorLogger("testMessagewith SEND");
+
+
+sendErrorLog("testMessagewith SEND");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
